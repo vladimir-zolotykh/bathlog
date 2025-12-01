cc5b82df99da34207aebd1fc43d35e9e31a91ffa
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-12-01 13:18:37 +0200

    Make AJAX changes

--- timestamp/views.py
+++ timestamp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.utils import timezone  # noqa F401
 from .models import LogEntry
+from django.http import HttpResponse
 
 
 def home(request):
@@ -8,7 +9,16 @@ def home(request):
         action = request.POST.get("action")
         if action in ["pee", "poo"]:
             LogEntry.objects.create(action=action)
-        return redirect("home")
+        return (
+            redirect("home")
+            if not request.headers.get("X-Requested-With")
+            else HttpResponse("")
+        )
 
-    entries = LogEntry.objects.all()[:50]  # show last 50 entries
+    entries = LogEntry.objects.all()[:50]
+
+    # If ?partial=1 → render only the log list
+    if request.GET.get("partial"):
+        return render(request, "timestamp/log_partial.html", {"entries": entries})
+
     return render(request, "timestamp/home.html", {"entries": entries})
